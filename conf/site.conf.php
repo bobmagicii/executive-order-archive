262f9d39cb469fc02297b37066269e0b244cc13f
bobmagicii <bob@majdak.net>
2017-01-25 14:17:39 -0600

    magic user agent...

--- conf/site.conf.php
+++ conf/site.conf.php
@@ -4,3 +4,20 @@ Nether\Option::Set([
 	'nether-web-root' => WebRoot,
 	'nether-web-path' => '/'
 ]);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// i have my reasons.
+
+ini_set('user_agent',sprintf(
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '.
+	'AppleWebKit/%d.%d (KHTML, like Gecko) '.
+	'Chrome/%d.%d.%d.%d Safari/%1$d.%2$d',
+	random_int(400,550),
+	random_int(1,99),
+	random_int(40,55),
+	random_int(1,9),
+	random_int(1000,2500),
+	random_int(1,99)
+));
